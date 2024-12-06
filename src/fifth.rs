1d1e52ad6b874f3cd027f31533b8fb79ae134e9b
Garry Cairns <garryjcairns@gmail.com>
2024-12-06 15:06:36 +0000

    Solves day 5 part 2

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,6 +1,5 @@
 #[path = "utils.rs"]
 mod utils;
-use itertools::Itertools;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
@@ -35,42 +34,61 @@ impl RuleSet {
         }
         right_values
     }
+
+    fn sort_rules(&self, page: &Vec<u32>) -> Vec<u32> {
+        let mut ordering: HashMap<usize, u32> = HashMap::new();
+        let mut result: Vec<u32> = Vec::new();
+        for item in page {
+            let mut remaining_set = HashSet::from_iter(page);
+            remaining_set.remove(item);
+            let lefts = self.lefts(*item);
+            let left_intersection: HashSet<&u32> =
+                lefts.intersection(&remaining_set).cloned().collect();
+            let position = left_intersection.len();
+            ordering.insert(position, *item);
+        }
+        let mut keys: Vec<usize> = ordering.keys().copied().collect();
+        keys.sort();
+        for key in keys {
+            result.push(*ordering.get(&key).unwrap());
+        }
+        println!(
+            "Page: {:?}, Sorted: {:?}, Ordering: {:?}",
+            page, &result, &ordering
+        );
+        result
+    }
 }
 
 pub fn pageorderingtotal(input: &str) -> u32 {
     let sections: Vec<&str> = input.split("\n\n").collect();
-    let rules = utils::string_to_2d_array(sections[0], make_rules).concat();
+    let rules = RuleSet {
+        rules: utils::string_to_2d_array(sections[0], make_rules).concat(),
+    };
     let pages = utils::string_to_2d_array(sections[1], extract_page_numbers);
     pages.iter().fold(0, |acc, x| acc + is_valid(&x, &rules))
 }
 
 pub fn corrected_total(input: &str) -> u32 {
     let sections: Vec<&str> = input.split("\n\n").collect();
-    let rules = utils::string_to_2d_array(sections[0], make_rules).concat();
+    let rules = RuleSet {
+        rules: utils::string_to_2d_array(sections[0], make_rules).concat(),
+    };
     let pages = utils::string_to_2d_array(sections[1], extract_page_numbers);
     pages.iter().fold(0, |acc, x| acc + valid_perm(&x, &rules))
 }
 
-fn valid_perm(page: &Vec<u32>, rules: &Vec<Rule>) -> u32 {
+fn valid_perm(page: &Vec<u32>, rules: &RuleSet) -> u32 {
     if is_valid(page, rules) != 0 {
         return 0;
     } else {
-        let relevant = relevant_rules(page, &rules);
-        for (i, key) in page.iter().enumerate() {
-            let lefts = relevant.lefts(*key);
-            let left_set = HashSet::from_iter(&page[..i]);
-            let left_intersection: HashSet<&u32> = lefts.intersection(&left_set).cloned().collect();
-            let rights = relevant.rights(*key);
-            let right_set = HashSet::from_iter(&page[i + 1..]);
-            let right_intersection: HashSet<&u32> =
-                rights.intersection(&right_set).cloned().collect();
-            return 1;
-        }
+        let mut relevant = relevant_rules(page, rules);
+        let sorted = &relevant.sort_rules(page);
+        return sorted[sorted.len() / 2];
     }
-    return 0;
 }
 
-fn is_valid(page: &Vec<u32>, rules: &Vec<Rule>) -> u32 {
+fn is_valid(page: &Vec<u32>, rules: &RuleSet) -> u32 {
     let relevant = relevant_rules(page, &rules);
     for (i, key) in page.iter().enumerate() {
         let lefts = relevant.lefts(*key);
@@ -100,24 +118,15 @@ fn make_rules(original: &str) -> Vec<Rule> {
     .to_vec()
 }
 
-fn relevant_rules(page: &Vec<u32>, rules: &Vec<Rule>) -> RuleSet {
+fn relevant_rules(page: &Vec<u32>, rules: &RuleSet) -> RuleSet {
     let mut relevant: Vec<Rule> = Vec::new();
-    let mut left: HashMap<u32, Vec<Rule>> = HashMap::new();
-    let mut right: HashMap<u32, Vec<Rule>> = HashMap::new();
-    for rule in rules {
-        left.entry(rule.left.clone())
-            .or_insert_with(Vec::new)
-            .push(rule.clone());
 
-        right
-            .entry(rule.right.clone())
-            .or_insert_with(Vec::new)
-            .push(rule.clone());
+    for rule in &rules.rules {
+        if page.contains(&rule.left) && page.contains(&rule.right) {
+            relevant.push(rule.clone());
+        }
     }
-    for item in page {
-        relevant.extend(left.get(&item).unwrap_or(&vec![]).clone());
-        relevant.extend(right.get(&item).unwrap_or(&vec![]).clone());
-    }
+
     RuleSet { rules: relevant }
 }
 
