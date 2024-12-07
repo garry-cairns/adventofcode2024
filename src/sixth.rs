e435c2595879c26220abe52b2932d40e858e17b3
Garry Cairns <garryjcairns@gmail.com>
2024-12-07 13:59:15 +0000

    Uses ndarray for regularly shaped arrays

--- src/sixth.rs
+++ src/sixth.rs
@@ -1,5 +1,6 @@
 #[path = "utils.rs"]
 mod utils;
+use ndarray::Array2;
 use std::collections::HashSet;
 
 #[derive(Debug, Default)]
@@ -15,59 +16,64 @@ struct GameState {
 }
 
 pub fn guard_path(input: &str) -> u32 {
-    let grid = utils::string_to_2d_array(input, utils::just_chars);
+    let mut visited: HashSet<utils::CoOrd> = HashSet::new();
+    let grid = utils::vec_to_array2(utils::string_to_2d_array(input, utils::just_chars));
+    let mut directions = vec![(-1, 0), (0, 1), (-1, 0), (0, -1)].into_iter().cycle();
     let cursor = find_guard(&grid);
-    let path: Vec<utils::CoOrd> = Vec::new();
-    walk(&grid, &cursor, &path)
-        .iter()
-        .collect::<HashSet<_>>()
-        .len() as u32
+    visited.insert(cursor);
+    let obstacles = utils::locate_all_in_grid(&grid, &'#');
+    // The point here is to avoid hitting all the squares
+    // We know where the guard is and whether it's travelling up, down, left or right
+    // Since we know where the obstables are we can add any squares not already
+    // in the visited set between the guard and the next obstacle immediately,
+    // and then change direction and repeat.
+    3
 }
 
 pub fn cycles(input: &str) -> u32 {
     3
 }
 
-fn walk(
-    grid: &Vec<Vec<char>>,
-    cursor: &utils::CoOrd,
-    path: &Vec<utils::CoOrd>,
-) -> Vec<utils::CoOrd> {
-    let mut new_path: Vec<utils::CoOrd> = path.iter().copied().collect();
-    new_path.push(cursor.clone());
-    let proposed_move = next(grid, cursor);
-    match proposed_move {
-        Some(coord) => {
-            let updated = update_grid(grid, &coord);
-            new_path.extend(walk(&updated.grid, &updated.guard.location, &new_path));
-        }
-        None => println!("Stopped"),
-    }
-    new_path
-}
-
-fn update_grid(grid: &Vec<Vec<char>>, proposed: &utils::CoOrd) -> GameState {
-    let mut new_grid = grid.clone();
-    let guard_at_start = find_guard(&new_grid);
-    let mut guard_at_end: Guard = Guard::default();
-
-    if grid[proposed.i][proposed.j] == '#' {
-        guard_at_end.location = guard_at_start;
-        guard_at_end.orientation = turn(&new_grid[guard_at_start.i][guard_at_start.j]);
-        new_grid[guard_at_start.i][guard_at_start.j] = guard_at_end.orientation;
-    } else {
-        new_grid[proposed.i][proposed.j] = new_grid[guard_at_start.i][guard_at_start.j];
-        guard_at_end.location = proposed.clone();
-        new_grid[guard_at_start.i][guard_at_start.j] = 'X';
-    }
-
-    GameState {
-        grid: new_grid,
-        guard: guard_at_end,
-    }
-}
-
-fn find_guard(grid: &Vec<Vec<char>>) -> utils::CoOrd {
+//fn walk(
+//    grid: &Vec<Vec<char>>,
+//    cursor: &utils::CoOrd,
+//    path: &Vec<utils::CoOrd>,
+//) -> HashMap<utils::CoOrd> {
+//    let mut new_path: Vec<utils::CoOrd> = path.iter().copied().collect();
+//    new_path.push(cursor.clone());
+//    let proposed_move = next(grid, cursor);
+//    match proposed_move {
+//        Some(coord) => {
+//            let updated = update_grid(grid, &coord);
+//            new_path.extend(walk(&updated.grid, &updated.guard.location, &new_path));
+//        }
+//        None => println!("Stopped"),
+//    }
+//    new_path
+//}
+//
+//fn update_grid(grid: &Vec<Vec<char>>, proposed: &utils::CoOrd) -> GameState {
+//    let mut new_grid = grid.clone();
+//    let guard_at_start = find_guard(&new_grid);
+//    let mut guard_at_end: Guard = Guard::default();
+//
+//    if grid[proposed.i][proposed.j] == '#' {
+//        guard_at_end.location = guard_at_start;
+//        guard_at_end.orientation = turn(&new_grid[guard_at_start.i][guard_at_start.j]);
+//        new_grid[guard_at_start.i][guard_at_start.j] = guard_at_end.orientation;
+//    } else {
+//        new_grid[proposed.i][proposed.j] = new_grid[guard_at_start.i][guard_at_start.j];
+//        guard_at_end.location = proposed.clone();
+//        new_grid[guard_at_start.i][guard_at_start.j] = 'X';
+//    }
+//
+//    GameState {
+//        grid: new_grid,
+//        guard: guard_at_end,
+//    }
+//}
+//
+fn find_guard(grid: &Array2<char>) -> utils::CoOrd {
     let chars: Vec<char> = vec!['^', '>', '∨', '<'];
     for guard in chars {
         let possible = utils::locate_in_grid(grid, &guard);
@@ -77,53 +83,53 @@ fn find_guard(grid: &Vec<Vec<char>>) -> utils::CoOrd {
     }
     return utils::CoOrd { i: 0, j: 0 };
 }
-
-fn next(grid: &Vec<Vec<char>>, cursor: &utils::CoOrd) -> Option<utils::CoOrd> {
-    let i_max = grid.len();
-    let j_max = grid[0].len();
-    if grid[cursor.i][cursor.j] == '^' {
-        if cursor.i >= 1 {
-            return Some(utils::CoOrd {
-                i: cursor.i - 1,
-                j: cursor.j,
-            });
-        }
-    } else if grid[cursor.i][cursor.j] == '>' {
-        if cursor.j + 1 < j_max {
-            return Some(utils::CoOrd {
-                i: cursor.i,
-                j: cursor.j + 1,
-            });
-        }
-    } else if grid[cursor.i][cursor.j] == '∨' {
-        if cursor.i + 1 < i_max {
-            return Some(utils::CoOrd {
-                i: cursor.i + 1,
-                j: cursor.j,
-            });
-        }
-    } else {
-        if cursor.j >= 1 {
-            return Some(utils::CoOrd {
-                i: cursor.i,
-                j: cursor.j - 1,
-            });
-        }
-    }
-    return None;
-}
-
-fn turn(guard: &char) -> char {
-    if guard == &'^' {
-        '>'
-    } else if guard == &'>' {
-        '∨'
-    } else if guard == &'∨' {
-        '<'
-    } else {
-        '^'
-    }
-}
+//
+//fn next(grid: &Vec<Vec<char>>, cursor: &utils::CoOrd) -> Option<utils::CoOrd> {
+//    let i_max = grid.len();
+//    let j_max = grid[0].len();
+//    if grid[cursor.i][cursor.j] == '^' {
+//        if cursor.i >= 1 {
+//            return Some(utils::CoOrd {
+//                i: cursor.i - 1,
+//                j: cursor.j,
+//            });
+//        }
+//    } else if grid[cursor.i][cursor.j] == '>' {
+//        if cursor.j + 1 < j_max {
+//            return Some(utils::CoOrd {
+//                i: cursor.i,
+//                j: cursor.j + 1,
+//            });
+//        }
+//    } else if grid[cursor.i][cursor.j] == '∨' {
+//        if cursor.i + 1 < i_max {
+//            return Some(utils::CoOrd {
+//                i: cursor.i + 1,
+//                j: cursor.j,
+//            });
+//        }
+//    } else {
+//        if cursor.j >= 1 {
+//            return Some(utils::CoOrd {
+//                i: cursor.i,
+//                j: cursor.j - 1,
+//            });
+//        }
+//    }
+//    return None;
+//}
+//
+//fn turn(guard: &char) -> char {
+//    if guard == &'^' {
+//        '>'
+//    } else if guard == &'>' {
+//        '∨'
+//    } else if guard == &'∨' {
+//        '<'
+//    } else {
+//        '^'
+//    }
+//}
 
 #[cfg(test)]
 mod tests {
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use log::debug;
+use ndarray::Array2;
 use reqwest::blocking::get;
 use reqwest::Error;
 use std::fs::File;
@@ -28,6 +29,15 @@ pub fn file_input(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
+pub fn vec_to_array2<T>(vec_of_vecs: Vec<Vec<T>>) -> Array2<T>
+where
+    T: Clone,
+{
+    let flattened: Vec<T> = vec_of_vecs.clone().into_iter().flatten().collect();
+    let rows = flattened.len() / vec_of_vecs.len();
+    Array2::from_shape_vec((vec_of_vecs.len(), rows), flattened).unwrap()
+}
+
 pub fn string_to_2d_array<F, T>(input: &str, manipulation: F) -> Vec<Vec<T>>
 where
     F: Fn(&str) -> Vec<T>,
@@ -41,19 +51,25 @@ pub struct CoOrd {
     pub j: usize,
 }
 
-// Assumes you're looking for exactly one `target`
-pub fn locate_in_grid<T>(grid: &Vec<Vec<T>>, target: &T) -> Option<CoOrd>
+// Assumes you're looking for all matches of `target`
+pub fn locate_all_in_grid<T>(grid: &Array2<T>, target: &T) -> Vec<CoOrd>
 where
-    T: Eq,
+    T: PartialEq,
 {
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            if grid[i][j] == *target {
-                return Some(CoOrd { i: i, j: j });
-            }
-        }
-    }
-    None
+    grid.indexed_iter()
+        .filter(|(_, value)| *value == target)
+        .map(|((i, j), _)| CoOrd { i, j })
+        .collect()
+}
+
+// Assumes you're looking for exactly one `target`
+pub fn locate_in_grid<T>(grid: &Array2<T>, target: &T) -> Option<CoOrd>
+where
+    T: PartialEq,
+{
+    grid.indexed_iter()
+        .find(|(_, value)| *value == target)
+        .map(|((i, j), _)| CoOrd { i, j })
 }
 
 pub fn just_chars(string: &str) -> Vec<char> {
@@ -115,12 +131,24 @@ treb7uchet"#;
     }
 
     #[test]
+    fn test_find_all_in_grid() {
+        let input = r#".....
+.g...
+...g.
+....."#;
+        let grid = vec_to_array2(string_to_2d_array(input, just_chars));
+        let result = locate_all_in_grid(&grid, &'g');
+
+        assert_eq!(result, vec![CoOrd { i: 1, j: 1 }, CoOrd { i: 2, j: 3 }]);
+    }
+
+    #[test]
     fn test_find_in_grid() {
         let input = r#".....
 .....
 ...g.
 ....."#;
-        let grid = string_to_2d_array(input, just_chars);
+        let grid = vec_to_array2(string_to_2d_array(input, just_chars));
         let result = locate_in_grid(&grid, &'g');
 
         assert_eq!(result.unwrap(), CoOrd { i: 2, j: 3 });
