78df77215c6544b47f36493bfc3c56f93741d3cf
Garry Cairns <garryjcairns@gmail.com>
2024-12-11 22:52:09 +0000

    Day 9 part two tests passing but input doesn't :-(

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod utils;
 fn main() {
     env_logger::init();
     let input = utils::file_input("./src/input.txt").unwrap();
-    let result = ninth::checksum(&input);
+    let result = ninth::whole_files(&input);
     println!("{result:?}");
 }
--- src/ninth.rs
+++ src/ninth.rs
@@ -1,9 +1,10 @@
 #[path = "utils.rs"]
 mod utils;
+use std::collections::HashSet;
 
 pub fn checksum(input: &str) -> usize {
-    let (mut big_string, check_size) = clean(input);
-    let mut backfills: Vec<String> = big_string
+    let (big_string, check_size) = clean(input);
+    let backfills: Vec<String> = big_string
         .clone()
         .iter()
         .cloned()
@@ -36,25 +37,65 @@ pub fn whole_files(input: &str) -> usize {
         .iter()
         .cloned()
         .filter(|s| *s != ".")
-        .rev()
         .collect();
-    let mut backfill_index = 0;
-    let mut checksum: Vec<usize> = Vec::new();
-    for (i, _) in (0..check_size).enumerate() {
-        let existing = big_string.get(i).unwrap().parse();
-        match existing {
-            Ok(num) => checksum.push(num),
-            Err(_) => {
-                let backfill: usize = backfills.get(backfill_index).unwrap().parse().unwrap();
-                checksum.push(backfill);
-                backfill_index += 1;
+    let distinct: HashSet<String> = backfills.iter().cloned().collect();
+    let mut distinct_ordered: Vec<String> = distinct.into_iter().collect();
+    distinct_ordered.sort_by(|a, b| b.cmp(a));
+    for fig in distinct_ordered {
+        let first_value_index = big_string.iter().position(|s| *s == fig);
+        match first_value_index {
+            Some(value_index) => {
+                let backfill: Vec<String> = backfills
+                    .iter()
+                    .filter(|&s| *s == fig)
+                    .map(|s| s.clone())
+                    .collect();
+                if backfill.len() > 0 {
+                    let first_fill_index = big_string
+                        .windows(backfill.len())
+                        .position(|window| window.iter().all(|s| s == "."));
+                    match first_fill_index {
+                        Some(fill_index) => {
+                            if fill_index < value_index {
+                                let matching_indices: Vec<usize> = big_string
+                                    .windows(backfill.len())
+                                    .enumerate()
+                                    .filter(|(i, window)| *window == backfill.as_slice())
+                                    .map(|(i, _)| i)
+                                    .collect();
+                                if matching_indices.len() == 1 {
+                                    big_string.splice(
+                                        fill_index..(fill_index + backfill.len()),
+                                        backfill.clone(),
+                                    );
+                                    let replacement = vec![String::from("."); backfill.len()];
+                                    big_string.splice(
+                                        matching_indices[0]..(matching_indices[0] + backfill.len()),
+                                        replacement,
+                                    );
+                                }
+                            }
+                        }
+                        None => {}
+                    }
+                }
             }
+            None => {}
         }
     }
-    checksum
+    big_string
         .iter()
         .enumerate()
-        .fold(0, |acc, (index, &value)| acc + index * value)
+        .fold(0, |acc, (index, &ref value)| {
+            acc + safe_product(index, value.to_string())
+        })
+}
+
+fn safe_product(index: usize, value: String) -> usize {
+    match value.parse::<usize>() {
+        Ok(i) => index * i,
+        Err(_) => 0,
+    }
 }
 
 fn clean(input: &str) -> (Vec<String>, usize) {
@@ -106,7 +147,23 @@ mod tests {
     fn test_whole_files() {
         let input = r#"2333133121414131402"#;
 
-        let result = checksum(input);
+        let result = whole_files(input);
         assert_eq!(result, 2858);
     }
+
+    #[test]
+    fn test_whole_files_again() {
+        let input = r#"2333133121414131401"#;
+
+        let result = whole_files(input);
+        assert_eq!(result, 2746);
+    }
 }
+
+/*
+00...111...2...333.44.5555.6666.777.888899
+0099.111...2...333.44.5555.6666.777.8888..
+0099.1117772...333.44.5555.6666.....8888..
+0099.111777244.333....5555.6666.....8888..
+00992111777.44.333....5555.6666.....8888..
+*/
